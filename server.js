de691481774470ddeaf8532ed5192fc92e5ce95e
andrey <captain.destroyer@yandex.ru>
2020-04-07 11:28:41 +0700

    Статичные файлы для сервера + правки скриптов в package.json

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || config.get('port');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+async function start() {
+  try {
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch(e) {
+    console.error('Server naebnulsa', e);
+  }
+}
+
+start();
