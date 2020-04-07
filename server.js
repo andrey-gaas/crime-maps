9be5a6e9c634b55f449f3b1074fa147f2aa6f5b7
andrey <captain.destroyer@yandex.ru>
2020-04-07 11:01:45 +0700

    Добавил клиент

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || config.get('port');
 
 app.get('/', (req, res) => {
   res.send('hello world');
-});
+})
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
