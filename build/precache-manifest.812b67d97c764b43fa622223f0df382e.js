4037565324bf99db04baf7b544d4357b7cee0c0c
andrey <captain.destroyer@yandex.ru>
2020-04-09 01:19:47 +0700

    Убрал авто-сборку

---
+++ build/precache-manifest.812b67d97c764b43fa622223f0df382e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b6569c3cedc3bee3cb9ea287dea5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d9757c8457b27c18ed5",
+    "url": "/static/css/main.331cf375.chunk.css"
+  },
+  {
+    "revision": "d20b0e8764cacadc9999",
+    "url": "/static/js/2.a3826b30.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/static/js/2.a3826b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9757c8457b27c18ed5",
+    "url": "/static/js/main.dcf1c240.chunk.js"
+  },
+  {
+    "revision": "f938d7bac29684014da5",
+    "url": "/static/js/runtime-main.ab553200.js"
+  },
+  {
+    "revision": "6ea331649600a20b623b16e340d36063",
+    "url": "/static/media/logo_dark.6ea33164.svg"
+  },
+  {
+    "revision": "2c7e8a940a21603d1913040ae641eb9b",
+    "url": "/static/media/novosibirsk-gerb.2c7e8a94.svg"
+  },
+  {
+    "revision": "3223fedefb3208849852a64bdbcc3adb",
+    "url": "/static/media/novosibirsk.3223fede.jpg"
+  }
+]);
