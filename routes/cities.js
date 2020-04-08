bb35e6b6ac5bbd3170bfcdacf4a1b2dd336b5f6d
andrey <captain.destroyer@yandex.ru>
2020-04-09 04:47:36 +0700

    Добавление, получение города из базы данных и отправка по api

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,46 @@
 const { Router } = require('express');
+const City = require('../models/City');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send(['Новосибирск', 'Москва', 'Ставрополь'])
+/* /api/cities/ */
+router.get('/', async (req, res) => {
+  try {
+    const cities = await City.find({});
+    res.send(cities);
+  } catch(e) {
+    console.log(e.message);
+    process.exit();
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, name } = req.body;
+    const cities = await City.find();
+
+    const candidate = cities.find(city => city.name === name);
+
+    if (candidate) {
+      return res.status(400).json({ message: 'Город уже существует' });
+    }
+
+    const id = cities.length + 1;
+
+    const city = new City({
+      id,
+      title,
+      name
+    });
+
+    await city.save();
+    res.send({ id, title, name });
+
+  } catch(e) {
+    console.log(e.message);
+    process.exit();
+    
+  }
 });
 
 module.exports = router;
