ab36f20f766284eab1f37bbb54401b625069e62d
andrey <captain.destroyer@yandex.ru>
2020-04-18 04:41:58 +0700

    оживил кнопку показа положения пользователя

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Fab } from '@material-ui/core';
 import { NearMe as NearMeIcon } from '@material-ui/icons';
 import Menu from '../../components/Menu';
 import Map from '../../components/Map';
 import CitiesSelector from '../../components/CitiesSelector';
 import useStyles from './styles';
+import { changeCoordinates } from '../../store/AC/map';
 
-function Main() {
+function Main(props) {
+  const {
+    changeCoordinates,
+    geodata,
+  } = props;
   const classes = useStyles();
+
+  console.log(geodata);
 
   return (
     <div className={classes.root}>
@@ -18,6 +28,8 @@ function Main() {
         color="primary"
         size="large"
         className={classes.buttonUserPosition}
+        onClick={() => changeCoordinates(geodata.coords.latitude, geodata.coords.longitude)}
+        disabled={geodata === null}
       >
         <NearMeIcon />
       </Fab>
@@ -25,4 +37,15 @@ function Main() {
   );
 }
 
-export default Main;
+Main.propTypes = {
+  changeCoordinates: PropTypes.func.isRequired,
+  geodata: PropTypes.object,
+};
+
+const mapStateToProps = ({ geodata }) => ({ geodata: geodata.geodata });
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changeCoordinates,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
