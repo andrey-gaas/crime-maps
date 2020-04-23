52409052dacc1f723b9747677c3c9018bd641041
andrey <captain.destroyer@yandex.ru>
2020-04-24 04:44:52 +0700

    post на создание происшествия

--- routes/incidents.js
+++ routes/incidents.js
@@ -3,6 +3,10 @@ const Incident = require('../models/Incident');
 
 const router = Router();
 
+function isUndefined(value) {
+  return value === undefined;
+}
+
 router.get('/', async (req, res) => {
   try {
     const incidents = await Incident.find({});
@@ -23,6 +27,43 @@ router.get('/:id', async (req, res) => {
     res.send(incident);
   } catch(e) {
     res.status(500).send({ error: e.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+
+  try {
+    const { type, title, text, coords, sources, media } = req.body;
+
+    if (
+      isUndefined(type) ||
+      isUndefined(title) ||
+      isUndefined(text) ||
+      isUndefined(coords) ||
+      isUndefined(sources) ||
+      isUndefined(media)
+    ) {
+      throw new SyntaxError('object is invalid');
+    }
+
+    const incidents = await Incident.find({});
+
+    const incindent = new Incident({
+      id: incidents.length + 1,
+      views: 0,
+      type,
+      title,
+      text,
+      coords,
+      sources,
+      media,
+    });
+
+    await incindent.save();
+    res.send('OK');
+
+  } catch(e) {
+    res.status(500).send(e.message);
   }
 });
 
