5387c607f6096b6b5b0df32a253e7e4866eba7d2
andrey <captain.destroyer@yandex.ru>
2020-04-24 02:31:45 +0700

    Добавил схему Incedent

---
+++ models/Incident.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  type: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: Number,
+    required: true,
+  },
+  coords: {
+    type: Array,
+    required: true,
+  },
+  sources: {
+    type: Array,
+    required: true,
+  },
+  media: {
+    type: Array,
+    required: true,
+  }
+});
+
+module.exports = model('Incedent', schema);
