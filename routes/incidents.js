b13679973637317c263f5fd1094481645ac7fe84
andrey <captain.destroyer@yandex.ru>
2020-04-24 03:27:52 +0700

    get для происшествий

--- routes/incidents.js
+++ routes/incidents.js
@@ -3,8 +3,27 @@ const Incident = require('../models/Incident');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Incedents');
+router.get('/', async (req, res) => {
+  try {
+    const incidents = await Incident.find({});
+    res.send(incidents);
+  } catch(e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const incident = await Incident.find({ id: req.params.id });
+    
+    if (!incident.length) {
+      throw new Error('not found');
+    }
+
+    res.send(incident);
+  } catch(e) {
+    res.status(500).send({ error: e.message });
+  }
 });
 
 module.exports = router;
