6e62c1f37e40c06e73bc5f651450e162cb87a7ed
andrey <captain.destroyer@yandex.ru>
2020-05-18 20:03:22 +0700

    регистрация. проверка полей. уникальность пользователя

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+  },
+});
+
+module.exports = model('User', schema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,33 @@
 const { Router } = require('express');
+const emailValidator = require('email-validator');
+const User = require('../models/User');
 
 const router = Router();
 
-router.post('/sign-up', (req, res) => {
-  console.log(req.body);
-  res.send('OK');
+router.post('/sign-up', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || name.length < 6 || name.length > 16) res.status(400).send('Имя должно содержать от 6 до 16 символов.');
+    if (!name.match(/^\s*([0-9a-zA-Zа-яА-Я]*)\s*$/)) res.status(400).send('Допустимы только латинские и кириллические символы, а так же числа.');
+    if (!emailValidator.validate(email)) res.status(400).send('Введите корректный E-Mail.');
+    if (!password || password.length < 6 || password.length > 16) res.status(400).send('Пароль должен содержать от 6 до 16 символов.');
+    if (!password.match(/^\s*([0-9a-zA-Z]*)\s*$/)) res.status(400).send('Допустимы только латинские символы и числа.');
+
+    const candidate = await User.findOne({
+      $or: [ { email }, { name } ],
+    });
+
+    if (candidate) {
+      if (candidate.name === name) res.status(409).send('Имя уже занято. Попробуйте другое.');
+      else res.status(409).send('Указанный E-Mail уже есть в системе. Попробуйте авторизоваться.')
+    }
+
+    res.send('ok');
+
+  } catch (e) {
+    res.status(500).send('server error');
+  }
 });
 
 module.exports = router;
