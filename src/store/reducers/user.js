f73dd90b72c14302ea97545c9560572e7d4075c5
andrey <captain.destroyer@yandex.ru>
2020-05-22 18:04:57 +0700

    убрал проверку токена

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,7 +1,16 @@
+import cookie from 'js-cookie';
 import { SET_USER, LOGOUT_USER } from '../actions/user';
 
+const token = cookie.get('token');
+const name = cookie.get('user-name');
+const email = cookie.get('user-email');
+const id = cookie.get('user-id');
+
 const initialState = {
-  isAuth: false,
+  isAuth: !!token || false ,
+  name:   name || null,
+  email:  email || null,
+  id:     id || null
 };
 
 export default function(state = initialState, action) {
