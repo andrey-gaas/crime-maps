20bd24f0a5c8f6648939b0d809e740b9ec924d89
andrey <captain.destroyer@yandex.ru>
2020-06-29 16:28:31 +0700

    Скрытие десктоп меню на маленьких экранах

--- src/components/DesctopMenu/styles.js
+++ src/components/DesctopMenu/styles.js
@@ -3,33 +3,39 @@ import backgrounds from '../../other/cityBackgrounds';
 import { WIDTH_MENU as drawerWidth } from '../../constants/styles';
 
 export default makeStyles(theme => ({
-  ...backgrounds,
   root: {
-    width: drawerWidth,
-    flexShrink: 0
+    display: 'none',
   },
-  rootPaper: {
-    width: drawerWidth
-  },
-  header: {
-    position: 'relative',
-    height: 200,
-  },
-  searchContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: theme.spacing(3),
-    padding: '5px 10px',
-    backgroundColor: '#fff',
-    borderRadius: theme.shape.borderRadius * 2,
-    border: '1px solid #ccc',
-  },
-  search: {
-    width: '86%'
-  },
-  blackout: {
-    paddingTop: 1,
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, .5)',
+  [theme.breakpoints.up('md')]: {
+    ...backgrounds,
+    root: {
+      display: 'block',
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    rootPaper: {
+      width: drawerWidth
+    },
+    header: {
+      position: 'relative',
+      height: 200,
+    },
+    searchContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      margin: theme.spacing(3),
+      padding: '5px 10px',
+      backgroundColor: '#fff',
+      borderRadius: theme.shape.borderRadius * 2,
+      border: '1px solid #ccc',
+    },
+    search: {
+      width: '86%'
+    },
+    blackout: {
+      paddingTop: 1,
+      height: '100%',
+      backgroundColor: 'rgba(0, 0, 0, .5)',
+    },
   },
 }));
