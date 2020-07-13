ba6b1cb18fe1c51f912a8e05f43a4099e954543f
andrey <captain.destroyer@yandex.ru>
2020-07-13 16:40:50 +0700

    компонент Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  title: {
+    marginLeft: theme.spacing(1),
+    fontSize: 18,
+    color: '#fff',
+  },
+}));
--- src/containers/Cabinet/index.js
+++ src/containers/Cabinet/index.js
@@ -2,11 +2,26 @@ import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
+import Header from '../../components/Header';
+import Menu from '../../components/ResponsiveMenu';
 import useStyles from './styles';
 
 function Cabinet(props) {
+  const [isOpen, setOpen] = useState(false);
+  const classes = useStyles();
+
   return (
-    <h1>Cabinet</h1>
+    <Fragment>
+      <Menu
+        isOpenMobile={isOpen}
+        onClose={() => setOpen(false)}
+      >
+        Menu
+      </Menu>
+      <div className={classes.content}>
+        <h1>CONTENT</h1>
+      </div>
+    </Fragment>
   );
 }
 
