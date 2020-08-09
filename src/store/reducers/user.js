48a0ff5fc8daf5c07aeaf459f1ba38535ac75a2f
andrey <captain.destroyer@yandex.ru>
2020-08-09 20:48:35 +0700

    роль пользователя при инициализации

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,10 +1,13 @@
 import cookie from 'js-cookie';
 import { SET_USER_DATA, LOGOUT_USER } from '../actions/user';
+import { ROLE_USER } from '../../constants/user';
 
 const token = cookie.get('token');
+const savedRole  = localStorage.getItem('role');
 
 const initialState = {
   isAuth: !!token,
+  role: savedRole !== null ? +savedRole : ROLE_USER,
 };
 
 export default function(state = initialState, action) {
